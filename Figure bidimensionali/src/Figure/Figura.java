7205c0470f392b2a2c1d538dbd5843bef4ba57a1
JuriVitali <S1092588@studenti.univpm.it>
2021-01-02 10:55:57 +0100

    Provo un commit

--- Figure bidimensionali/src/Figure/Figura.java
+++ Figure bidimensionali/src/Figure/Figura.java
@@ -4,7 +4,7 @@ public class Figura {
 	double area() {return 0;}
 	double perimetro() {return 0;}
 	public void get() {
-		System.out.println("Il perimetro misura " + perimetro());
+		System.out.println("Il perimetro è " + perimetro());
 		System.out.println("L'area misura " + area());
 	}
 }
